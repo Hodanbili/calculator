6115f8b81ccc05f6a287ba21c0598d04dd705661
Hodan Isse <hodanissah3@gmail.com>
2023-03-10 05:25:59 -0800

    this file is added code

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('buttons'));
 
-// const screenDisplay = document.querySelector('.screen')
-//  const buttons = document.querySelectorAll('button')
-
-// let calculation = []
-// let accumulativeCalculation
-
-// function calculate(){
-//     const value = button.textContent
-//     if (value ==="CLEAR"){
-//         calculation = []
-//         screenDisplay.textContent = '.'
-//     }
-//     else if (value ==="="){
-// screenDisplay.textContent = eval(accumulativeCalculation)
-//     }
-// else {
-//     calculation.push(value)
-//     accumulativeCalculation = calculation.join('')
-//     screenDisplay.textContent = calculation
-// }
-    
-// }
-
-//  buttons.forEach(buttons => buttons.addEventListener('clicks', () => calculate(button)))
-
-alert('Ok')
-console.log('Ok')
-
-
- function inputNumber(buttonId, inputNumberId){
-    var buttonNumber = document.getElementById(buttonId)
-    var inputNumber = document.getElementById(inputNumberId)
-    var previousNumber = document.getElementById(inputNumberId).innerHTML
-    // write number in the input
-    inputNumber.innerText = previousNumber + buttonNumber.innerHTML 
-    
- }
-
-
+buttons.map( button =>{
+   button.addEventListener('click', (e) =>{
+      console.log('clicked');
+      switch(e.target.innerText){
+         case 'C':
+            display.innerText = '';
+            break;
+            case '←':
+               if(display.innerText){
+                  display.innerText = display.innerText.slice(0, -1);
+               }
+               break;
+               case '=':
+                  try{
+                     display.innerText = eval(display.innerText);
+                  } catch {
+                     display.innerText = 'Error!';
+                  }
+                  
+                  break;
+         default:
+            display.innerText += e.target.innerText;
+      }
+      
+   });
+});
